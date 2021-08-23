6e8025d26d2b50fccbe28f1f0b4c4d3e9569b284
Emerson Almeida <emersonalmeidax@gmail.com>
2021-08-23 00:01:28 -0300

    add bunny cdn

---
+++ src/bunny-cdn.cr
@@ -0,0 +1,68 @@
+require "crest"
+require "json"
+
+module Bunny
+  class File
+    include JSON::Serializable
+
+    @[JSON::Field(key: "Guid")]
+    property guid : String
+
+    @[JSON::Field(key: "StorageZoneName")]
+    property storage_zone_name : String
+
+    @[JSON::Field(key: "Path")]
+    property path : String
+
+    @[JSON::Field(key: "ObjectName")]
+    property object_name : String
+
+    @[JSON::Field(key: "Length")]
+    property length : UInt32
+
+    @[JSON::Field(key: "LastChanged")]
+    property last_changed : String
+
+    @[JSON::Field(key: "IsDirectory")]
+    property is_directory : Bool
+
+    @[JSON::Field(key: "ServerId")]
+    property server_id : UInt32
+
+    @[JSON::Field(key: "UserId")]
+    property user_id : String
+
+    @[JSON::Field(key: "DateCreated")]
+    property date_created : String
+
+    @[JSON::Field(key: "StorageZoneId")]
+    property storage_zone_id : UInt32
+  end
+
+  class Cdn
+    VERSION      = "0.1.0"
+    ENDPOINT_URL = "https://storage.bunnycdn.com"
+
+    def initialize(@access_key : String)
+    end
+
+    def upload(storage_zone : String, path : String, file : IO | String) : HTTP::Client::Response
+       HTTP::Client.put(
+        "#{ENDPOINT_URL}/#{storage_zone}/#{path}",
+        headers: HTTP::Headers{"AccessKey" => @access_key},
+        form: file
+      )
+    end
+
+    def browser_files(storage_zone : String,
+                      path : String)
+      body = Crest.get(
+        "#{ENDPOINT_URL}/#{storage_zone}/#{path}/",
+        headers: {"AccessKey" => @access_key},
+        logging: true
+      ).body
+
+      Array(Bunny::File).from_json(body)
+    end
+  end
+end
